3965738014a439d0c100000318baad3335033c41
LAPTOP-JEREMY\Jeremy <krummenacherjeremy@gmail.com>
2022-06-09 09:05:47 +0200

    rechner

--- src/app/navigation-detail/navigation-detail.page.ts
+++ src/app/navigation-detail/navigation-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
-
+  navigateTo(link: string){
+    this.router.navigate(['/' + link]);
+  }
 }
